119cbb5bfe26708c2cdf11122ed18b910a116533
evilgenius <evilgenius1999@proton.me>
2024-04-19 00:56:22 +0800

    fix

--- src/main.js
+++ src/main.js
@@ -4,21 +4,23 @@ const Popup = require('./popup/popup.js')
 const Store = require('./store/store.js')
 
 let tray = null
+let store = new Store()
+let popup = null;
 
-app.on('ready', () => {
-    if (app.dock) {
-        app.dock.hide(); // 在MacOS上隐藏dock图标
+const updateItems = () => {
+
+    if (!tray) {
+        tray = new Tray(path.join(__dirname, 'icon.png'))
+        tray.setToolTip('QuickMenu')
     }
 
-    tray = new Tray(path.join(__dirname, 'icon.png'))
+    let storeItems = store.get()
+    let items = []
 
-    const store = new Store()
-    const popup = new Popup()
-
-    const contextMenu = Menu.buildFromTemplate([
-        { 
-            label: '添加选项', 
-            type: 'normal', 
+    items.push(
+        {
+            label: '添加选项',
+            type: 'normal',
             click: (menuItem, browserWindow, event) => {
                 if (event.shiftKey) {
 
@@ -29,22 +31,43 @@ app.on('ready', () => {
         },
         { label: '设置', type: 'normal' },
         { type: 'separator' },
-        { 
-            label: '按住 [SHIFT + 鼠标左键] 单击某一项进行编辑', 
+        {
+            label: '按住 [SHIFT + 鼠标左键] 单击某一项进行编辑',
             type: 'normal',
             enabled: false
-        },
-        { 
-            label: 'Item1Item1Item1', 
-            type: 'normal', 
-            click: () => {
-                clipboard.writeText('Example text');
+        }
+    )
+
+    for (let i = 0; i < storeItems.length; i++) {
+        let storeItem = storeItems[i];
+
+        items.push(
+            {
+                type: 'normal',
+                label: storeItem.label,
+                click: () => {
+                    clipboard.writeText(storeItem.clipboard);
+                }
             }
-        },
+        )
+    }
+
+    items.push(
         { type: 'separator' },
         { label: '退出', type: 'normal', click: () => app.quit() }
-    ])
+    )
 
-    tray.setToolTip('QuickMenu')
+    const contextMenu = Menu.buildFromTemplate(items)
+
     tray.setContextMenu(contextMenu)
+}
+
+app.on('ready', () => {
+    if (app.dock) {
+        app.dock.hide(); // 在MacOS上隐藏dock图标
+    }
+
+    popup = new Popup();
+
+    updateItems();
 })
--- src/store/store.js
+++ src/store/store.js
@@ -6,22 +6,64 @@ class Store {
         try {
             const items = fs.readFileSync(STORE_FILE_PATH, { encoding: 'utf8' });
 
-            this.items = JSON.parse(items);
+            if (items) {
+                this.items = JSON.parse(items);
+            } else {
+                this.initFile()
+            }
         } catch (error) {
             if (error.code === 'ENOENT') {
-                const defaultData = '[]';
-
-                console.log('首次使用, 初始化...');
-
-                fs.writeFileSync(STORE_FILE_PATH, defaultData, { encoding: 'utf8' });
-
-                this.items = JSON.parse(defaultData)
-
-                console.log('store创建完毕');
+                this.initFile()
             } else {
                 throw error;
             }
         }
+    }
+
+    initFile() {
+        const defaultData = '[]';
+
+        console.log('首次使用, 初始化...');
+
+        fs.writeFileSync(STORE_FILE_PATH, defaultData, { encoding: 'utf8' });
+
+        this.items = JSON.parse(defaultData)
+
+        console.log('store创建完毕');
+    }
+
+    push(item) {
+        item.id = this.createID()
+
+        this.items.push(item)
+
+        fs.writeFileSync(STORE_FILE_PATH, JSON.stringify(this.items), { encoding: 'utf8' });
+    }
+
+    get(id) {
+        let items = this.items;
+        let res;
+
+        if (!id) {
+            res = items
+        } else {
+            for (let i = 0; i < items.length; i++) {
+                let item = items[i];
+                if (item.id == id) {
+                    res = item
+                    break
+                }
+            }
+        }
+
+        return res
+    }
+
+    createID() {
+        const timestamp = Date.now().toString(36);
+        const randomString = Math.random().toString(36).substr(2, 5);
+        
+        return timestamp + randomString;
     }
 }
 
